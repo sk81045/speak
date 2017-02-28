58f38e22c65fa08e76ec7866d579ac380a7062a0
cult huang <sk81045@126.com>
2017-02-28 17:06:53 +0800

    2-28 tuday

--- TPI/script.js
+++ TPI/script.js
@@ -1,6 +1,32 @@
 
-function struct(){
-	 var en = $('#en').val();//取值
-    // var en = 545655;
-	alert(en);
+function senderAJAX(url,json){
+	$.ajax({
+       url : url,
+       type : 'POST',
+       dataType: 'json',
+       data: json,
+       async: false,//使用同步的方式,true为异步方式
+	   timeout: 1000,  
+       cache: false,  
+       beforeSend: false, //加载执行方法    
+       error: '',  //错误执行方法 
+       success : function(data){
+          call = data;
 }
+});
+      return call;
+}
+var url = "http://lonelygod/Router/speak/app.php";
+$(function(){
+	$("#gt-submit").click(function(){
+		var json = {
+			"content":$('#en').val()
+	    }; 
+		var call = senderAJAX(url,json);
+		if(call){
+		    $('#de').html("chinglish  :  "+"'"+call.chinglish+"'");
+		    $('#english').html("english  :  "+"'"+call.english+"'");
+		    $('#sxx').css("border-style"," solid");
+		}
+	})
+})
--- app.php
+++ app.php
@@ -1,20 +1,14 @@
 <?php
-namespace speak;
+ namespace speak;
  // use speak\api;
-require __DIR__.'/API.php';
+ require __DIR__.'/API.php';
 
-$userKey = 'dfcacb6404295f9ed9e430f67b641a8e';
-$key2 = '0bceb4fc0d3c9900e74186583128b8af';
-$content = "很高兴";
-$language = "e2c";
-$url = 'http://howtospeak.org:443/api/'.$language.'?user_key='.$userKey.'&notrans=0&text='.$content;
+ $userKey = 'dfcacb6404295f9ed9e430f67b641a8e';
+ $key2 = '0bceb4fc0d3c9900e74186583128b8af';
+ $content = $_POST['content'];
+ $language = "e2c";
+ $url = 'http://howtospeak.org:443/api/'.$language.'?user_key='.$userKey.'&notrans=0&text='.$content;
 
  $callContent = MyCurl::cUrl($url); 
- print_r($callContent);
- echo "<h3>".$callContent['chinglish']."</h3>";
-
-
-
- // $ad = new io();
- // $ad = $ad->DC(78988);
- // echo $ad;
+ $callContent = json_encode($callContent);
+ echo $callContent;
